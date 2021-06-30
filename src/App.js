79908c20899259432b7c430fefe0a6415d4c9f12
Niels van den Dool <n.vandendool@student.tudelft.nl>
2021-07-01 00:03:33 +0200

    Remove basic template, add header and background

--- src/App.js
+++ src/App.js
@@ -4,20 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Header">
+        <h1 className="title">Welkom bij deze app.</h1>
+        <h2 className="subtitle">Dit is mijn eerste React app</h2>
+      </div>
     </div>
   );
 }
