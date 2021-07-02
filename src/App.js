115bf31031a5f379e31e5f52106891b7e4fbda4a
Niels van den Dool <n.vandendool@student.tudelft.nl>
2021-07-02 17:41:11 +0200

    Add form and eventhandlers that update state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, {useState} from 'react';
+import Form from './Form'
 function App() {
+  
+
+
+  
   return (
     <div className="App">
       <div className="Header">
-        <h1 className="title">Welkom bij deze app.</h1>
-        <h2 className="subtitle">Dit is mijn eerste React app</h2>
+        <h1 className="title">Moties.</h1>
+        <h2 className="subtitle">Dien jouw moties hier in.</h2>
+      </div>
+      <div className="FormContainer">
+        <p>Een motie is een formeel middel waarmee een lid van een vergadering een discussiepunt voor kan leggen aan een vergadering. Hiertoe dient de motie in stemming gebracht te worden op een vergadering. Alle leden van een vergadering kunnen een motie indienen. Een bestuur zal in het algemeen geen moties indienen omdat een bestuur door middel van het amenderen van een vergaderagenda discussiepunten in stemming kan brengen.</p>
+        <div className="FormHeader">
+          <h3>Vul onderstaande formulier in om een motie in te dienen.</h3>
+        </div>
+        <Form />
+        
+        
       </div>
     </div>
   );
---
+++ src/Form.js
@@ -0,0 +1,80 @@
+import React, {Component, useState} from 'react';
+import './Form.css';
+
+class Form extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            name: '',
+            motieTitel: '',
+            overwegende: '',
+            overwegendeOpt: '',
+            constaterende: '',
+            constaterendeOpt: '',
+            verzoekt: ''
+        }
+    }
+
+    
+
+    changeNameHandler = (event) => {
+        this.setState({name: event.target.value})
+    }
+    changeMotieTitelHandler = (event) => {
+        this.setState({motieTitel: event.target.value})
+    }
+    changeOverwegendeHandler = (event) => {
+        this.setState({overwegende: event.target.value})
+    }
+    changeOverwegendeOptHandler = (event) => {
+        this.setState({overwegendeOpt: event.target.value})
+    }
+    changeConstaterendeHandler = (event) => {
+        this.setState({constaterende: event.target.value})
+    }
+    changeConstaterendeOptHandler = (event) => {
+        this.setState({constaterendeOpt: event.target.value})
+    }
+    changeVerzoektHandler = (event) => {
+        this.setState({verzoekt: event.target.value})
+    }
+
+      render() {
+          return  (
+            <form className="Form">
+            <div>
+              <label>Naam (indiener)</label>
+              <input type="text" value={this.state.name} onChange={this.changeNameHandler}></input>
+            </div>
+            <div>
+              <label>Motie titel</label>
+              <input type="text" value={this.state.motieTitel} onChange={this.changeMotieTitelHandler}></input>
+            </div>
+            <div>
+              <label>Overwegende...</label>
+              <input type="text" value={this.state.overwegende} onChange={this.changeOverwegendeHandler}></input>
+            </div>
+            <div>
+              <label>Overwegende... (optioneel)</label>
+              <input type="text" value={this.state.overwegendeOpt} onChange={this.changeOverwegendeOptHandler}></input>
+            </div>
+            <div>
+              <label>Constaterende...</label>
+              <input type="text" value={this.state.constaterende} onChange={this.changeConstaterendeHandler}></input>
+            </div>
+            <div>
+              <label>Constaterende... (optioneel)</label>
+              <input type="text" value={this.state.constaterendeOpt} onChange={this.changeConstaterendeOptHandler}></input>
+            </div>
+            <div>
+              <label>Verzoekt...</label>
+              <input type="text" value={this.state.verzoekt} onChange={this.changeVerzoektHandler}></input>
+            </div>
+            <input type="submit" value="Indienen" />
+        </form>
+          )
+      }
+
+}
+
+export default Form;
